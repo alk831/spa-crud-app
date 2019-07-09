e8c2ffaf58f26c61dac7f3533dd8d5e530ce8146
alk831 <adam54111@gmail.com>
2019-07-09 20:36:25 +0200

    configure firebase & env variables

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import firebase from 'firebase';
+import firebaseConfig from './config/firebase';
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
@@ -47,6 +48,9 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: isDevelopment ? '[name].css' : '[name].[hash].css'
+    }),
+    new Dotenv({
+      safe: true
     })
   ],
   devServer: {
