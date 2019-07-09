e7b3bd88449b56abcc02b0ef93ee7dad667c3162
alk831 <adam54111@gmail.com>
2019-07-09 17:50:46 +0200

    move header to separate comp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 import { AuthorizationProvider } from './context/Authorization';
+import { Header } from './components/Header';
 
 export const App = () => {
   return (
     <Router>
       <AuthorizationProvider>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Logowanie</Link>
-            </li>
-          </ul>
-        </nav>
+        <Header />
         <Route path="/" exact component={Home} />
         <Route path="/login/" component={Login} />
       </AuthorizationProvider>
