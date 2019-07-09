e1d756436c21e3c870faa3ca488407415fe22ae0
alk831 <adam54111@gmail.com>
2019-07-09 16:31:33 +0200

    setup webpack config

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  entry: './src/main',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    chunkFilename: '[name].bundle.js',
+    filename: '[name].bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.scss']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: /src/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
+            options: { modules: true }
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: isDevelopment }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/public/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? '[name].css' : '[name].[hash].css'
+    })
+  ],
+  devServer: {
+    contentBase: './src',
+    hot: true
+  }
+}
