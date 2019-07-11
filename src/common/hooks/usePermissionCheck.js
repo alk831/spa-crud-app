73a368fb13269fdd5963015e4ea7f40b5863bf00
alk831 <adam54111@gmail.com>
2019-07-11 20:34:02 +0200

    create usePermissionCheck hook

---
+++ src/common/hooks/usePermissionCheck.js
@@ -0,0 +1,28 @@
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+
+export function usePermissionCheck({
+  allowedRole,
+  strictRole = false,
+}) {
+  const userRole = useSelector(state => state.authorization.user.role);
+  const roles = useSelector(state => state.authorization.roles);
+
+  const userRoleIndex = roles.findIndex(role => role === userRole);
+  const allowedRoleIndex = roles.findIndex(role => role === allowedRole);
+
+  if (userRole === allowedRole) {
+    return true;
+  }
+  
+  if (!strictRole && userRoleIndex >= allowedRoleIndex) {
+    return true;
+  }
+
+  return false;
+}
+
+usePermissionCheck.propTypes = {
+  allowedRole: PropTypes.any,
+  strictRole: PropTypes.bool
+}
