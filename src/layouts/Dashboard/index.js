0cfe87e7e9fc40a2df8c3752b0b4a5cf6aeaf625
alk831 <adam54111@gmail.com>
2019-07-23 12:37:05 +0200

    refactor Dashboard to class component

--- src/layouts/Dashboard/index.js
+++ src/layouts/Dashboard/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import css from './style.scss';
-import { useSelector, useDispatch } from 'react-redux';
-import * as Actions from '../../store/actions';
+import { logout } from '../../store/actions';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import { UserInfo } from '../../components/UserInfo';
 import { BasicButton } from '../../components/BasicButton';
@@ -13,38 +14,67 @@ const dashboardLinks = [
   { title: 'Użytkownicy', path: '/users' },
 ]
 
-export const Dashboard = ({ children }) => {
-  const dispatch = useDispatch();
-  const user = useSelector(state => state.authorization.user);
+class Dashboard extends React.Component {
+  
+  state = {
+    error: null
+  }
 
-  return (
-    <main className={css.container}>
-      <nav className={css.menu_container}>
-        <UserInfo user={user} />
-        <ul className={css.menu_list}>
-          {dashboardLinks.map((link, i) => (
-            <li key={i}>
-              <NavLink
-                className={css.menu_link}
-                activeClassName={css.menu_link__active}
-                to={link.path}
-                exact
-              >
-                {link.title}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-        <BasicButton
-          className={css.logout_btn}
-          onClick={() => dispatch(Actions.handleLogout())}
-        >
-          Wyloguj się
-        </BasicButton>
-      </nav>
-      <article className={css.content_container}>
-        {children}
-      </article>
-    </main>
-  );
+  static getDerivedStateFromError(error) {
+    return { error };
+  }
+
+  handleLogout = () => {
+    this.props.logout();
+    this.props.history.push('/login');
+  }
+
+  render() {
+    const { error } = this.state;
+
+    return (
+      <main className={css.container}>
+        <nav className={css.menu_container}>
+          <UserInfo user={this.props.user} />
+          <ul className={css.menu_list}>
+            {dashboardLinks.map((link, i) => (
+              <li key={i}>
+                <NavLink
+                  className={css.menu_link}
+                  activeClassName={css.menu_link__active}
+                  to={link.path}
+                  exact
+                >
+                  {link.title}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+          <BasicButton
+            className={css.logout_btn}
+            onClick={this.handleLogout}
+          >
+            Wyloguj się
+          </BasicButton>
+        </nav>
+        <article className={css.content_container}>
+          {error ? (
+            <div>
+              Wysątpił błąd: 
+              <p>{error.message}</p>
+            </div>
+          ) : this.props.children}
+        </article>
+      </main>
+    );
+  }
 }
+
+const ConnectedDashboard = connect(
+  ({ authorization }) => ({ user: authorization.user }),
+  { logout }
+)(Dashboard);
+
+const DashboardWithRouter = withRouter(ConnectedDashboard);
+
+export { DashboardWithRouter as Dashboard };
