3d2e6350401b1ac614ff84e95284ab6294845f9b
alk831 <adam54111@gmail.com>
2019-07-29 15:07:16 +0200

    add API_HOST env variable

--- src/common/consts.js
+++ src/common/consts.js
@@ -1,3 +1,3 @@
 export const NODE_ENV = process.env.NODE_ENV || 'production';
 export const isDevelopment = NODE_ENV === 'development';
-export const HOST = 'http://localhost:3000';
+export const HOST = isDevelopment ? 'http://localhost:3000' : process.env.API_HOST;
